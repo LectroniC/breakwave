393e927dfcc09bfcc10236f926e81166d7ecf566
LectroniC <lichengluo1998@gmail.com>
2021-10-27 18:16:32 -0500

    Fix errors.

--- adaptive_attack.py
+++ adaptive_attack.py
@@ -92,7 +92,7 @@ class Attack:
         waveguarded_pass_in = waveform_to_mel_to_waveform_tf(preprocess_passin, fs, NFFT, NHOP, mel_num_bins=mel_bins)
         transformed_pass_in = audioio.audio_postprocess_tf(waveguarded_pass_in)
 
-        self.logits_transformed = logits_transformed = get_logits(transformed_pass_in, lengths)
+        self.logits_transformed = logits_transformed = get_logits(transformed_pass_in, lengths, reuse=True)
 
         # And finally restore the graph to make the classifier
         # actually do something interesting.
@@ -336,7 +336,7 @@ def main():
                 finetune.append(list(wav.read(args.finetune[i])[1]))
 
         maxlen = max(map(len,audios))
-        
+
         # Padding multiple audio files in a batch to the same length
         audios = np.array([x+[0]*(maxlen-len(x)) for x in audios])
         finetune = np.array([x+[0]*(maxlen-len(x)) for x in finetune])
