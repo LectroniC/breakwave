3aa0aa9b62757f3b049cbf217185784bc4c21369
Licheng Luo <ll6@illinois.edu>
2021-12-02 15:33:57 -0600

    Use batch

--- randomized_smoothing_classify.py
+++ randomized_smoothing_classify.py
@@ -167,8 +167,9 @@ def main():
                 logits_smooth = np.mean(new_logits_ls_np, axis=1, keepdims=True)
                 final_logits_list.append(logits_smooth)
                 final_logits_holder = tf.placeholder(tf.float32, logits_smooth.shape)
-                final_decoded, _ = tf.nn.ctc_beam_search_decoder(final_logits_holder, lengths, merge_repeated=False, beam_width=500)
-                r = sess.run((final_decoded), {final_logits_holder: logits_smooth, lengths: [length]})
+                final_length_holder = tf.placeholder(tf.float32, [1])
+                final_decoded, _ = tf.nn.ctc_beam_search_decoder(final_logits_holder, final_length_holder, merge_repeated=False, beam_width=500)
+                r = sess.run((final_decoded), {final_logits_holder: logits_smooth, final_length_holder: [length]})
                 decoded_list.append(r)
                 sess.close()
 
